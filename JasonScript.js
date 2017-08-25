1f150e3ac43ae87f037f6b1a06862a7df6ff3135
JShweiri <31330210+JShweiri@users.noreply.github.com>
2017-08-25 09:03:54 -0700

    Again?

--- JasonScript.js
+++ JasonScript.js
@@ -1,56 +1,52 @@
-
-window.onload=function(){
-var canv = document.getElementById("cv");
-canv.addEventListener("onclick", pressed);
-var ctx = canv.getContext("2d");
-setInterval(game, 1000/15);
+window.onload=function() {
+	canv=document.getElementById("gc");
+	ctx=canv.getContext("2d");
+	canv.addEventListener("onclick",pressed);
+	setInterval(game,1000/15);
 }
-
-px=py=5;
+px=py=10;
 gs=tc=20;
 ax=ay=15;
 xv=yv=0;
-trail[];
-tail = 1;
+trail=[];
+tail = 5;
+function game() {
+	px+=xv;
+	py+=yv;
+	if(px<0) {
+		px= tc-1;
+	}
+	if(px>tc-1) {
+		px= 0;
+	}
+	if(py<0) {
+		py= tc-1;
+	}
+	if(py>tc-1) {
+		py= 0;
+	}
+	ctx.fillStyle="black";
+	ctx.fillRect(0,0,canv.width,canv.height);
 
-function game(){
-  px+=xv;
-  py+=yv;
-  if(px>tc-1){
-    px = 0;
-  }
-  if(px<0){
-    px = tc-1;
-  }
-  if(py>tc-1){
-    py = 0;
-  }
-  if(py<0){
-    py = tc-1;
-  }
-ctx.fillStyle="black";
-ctx.fillRect(0,0,canv.width,canv.height);
+	ctx.fillStyle="lime";
+	for(var i=0;i<trail.length;i++) {
+		ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
+		if(trail[i].x==px && trail[i].y==py) {
+			tail = 5;
+		}
+	}
+	trail.push({x:px,y:py});
+	while(trail.length>tail) {
+	trail.shift();
+	}
 
-ctx.fillStyle="Cyan";
-for (var i = 0; i<trail.length; i++){
-  ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
-  if(trail[i].x == px && trail[i].y == py){
-    tail = 1;
-    //Gameover Stuff
-  }
-}
-trail.push({x:px, y:py});
-while(trail.length>tail){
-  trail.shift();
-}
-
-if(ax==px && ay==py) {
-        tail++;
-        ax=Math.floor(Math.random()*tc);
-        ay=Math.floor(Math.random()*tc);
-    }
-    ctx.fillStyle="red";
-    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
+	if(ax==px && ay==py) {
+		tail++;
+		ax=Math.floor(Math.random()*tc);
+		ay=Math.floor(Math.random()*tc);
+	}
+	ctx.fillStyle="red";
+	ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
 }
 
  function pressed(){
