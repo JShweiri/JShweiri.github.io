f1263e4e28c0df308cfbad7ff889727ec4ea71f3
JShweiri <31330210+JShweiri@users.noreply.github.com>
2017-08-25 09:46:14 -0700

    faster?

--- JasonScript.js
+++ JasonScript.js
@@ -48,7 +48,7 @@ function game() {
 	ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
 }
 
-function pressed(event){
+gc.onclick = function pressed(event){
    var mx = event.clientX;
    var my = event.clientY;
   if(mx > 200 && xv ==0){
